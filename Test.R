7aa7bfd8e471201f30a14ffab2686e7747ab98f9
maurice60 <maurice.williams@me.com>
2014-08-16 17:39:42 +0100

    Update casualties

--- Test.R
+++ Test.R
@@ -1,1 +1,7 @@
-storm <- read.csv(bzfile('StormData.csv.bz2'))
+cas <- sqldf('select EVTYPE, fatalities, injuries, casualties
+                from byType
+                where casualties > 0
+                order by casualties')
+
+head(cas, 10)
+barplot(as.matrix(rbind(cas$fatalities, cas$injuries)), horiz=TRUE, names.arg=cas$EVTYPE)
