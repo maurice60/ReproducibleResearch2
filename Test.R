d7c049c49b4a398ea4af5fc4fc5a1695199b7bf3
maurice60 <maurice.williams@me.com>
2014-08-17 17:23:55 +0100

    Graphs

--- Test.R
+++ Test.R
@@ -1,7 +1,23 @@
-cas <- sqldf('select EVTYPE, fatalities, injuries, casualties
+eco <- sqldf('select EVTYPE, property, crops, damage
                 from byType
-                where casualties > 0
-                order by casualties')
+                where damage > 0
+             order by damage desc')
 
-head(cas, 10)
-barplot(as.matrix(rbind(cas$fatalities, cas$injuries)), horiz=TRUE, names.arg=cas$EVTYPE)
+topTypeEco <- eco[1:10,]
+
+ecoPlt <- sqldf('select EVTYPE evtype, damage, "Total" rtype
+      from topTypeEco')
+
+ecoPlt <- rbind(ecoPlt, sqldf('select EVTYPE evtype, property damage, "Property Damage" rtype
+      from topTypeEco'))
+
+ecoPlt <- rbind(ecoPlt, sqldf('select EVTYPE evtype, crops damage, "Crop Damage" rtype
+      from topTypeEco'))
+
+plt <- ggplot(data=ecoPlt, aes(x=evtype, y=damage, fill=factor(rtype))) + 
+    geom_bar(position="dodge", stat="identity") + coord_flip() +
+    scale_x_discrete(limits=rev(topTypeEco$EVTYPE)) +
+    theme(legend.title=element_blank(), legend.position=c(.75,.7))
+print(plt)
+
+
